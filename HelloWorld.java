61bd4a9c5cd5d35c54ad2c456dd856a512b757bb
mayuresh20 <mayuresh.khambekar@gmail.com>
2018-08-25 15:28:35 +0530

    modified helloworl

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello,world ");
+        System.out.println("Hello,world hi");
     }
 
 }
